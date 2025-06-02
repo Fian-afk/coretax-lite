7c7d88d3bed155703108487d5de56eae5997e5ff
fianafk <luthfianafif.2024@student.uny.ac.id>
2025-06-02 17:32:16 +0700

    menambah warna primary, frontend, luthfian

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary' : "#4f46e5",
+            }
         },
     },
 
