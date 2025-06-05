9214f24f14998fb25bd5fe0f307a7ebb06261686
fianafk <luthfianafif.2024@student.uny.ac.id>
2025-06-05 16:07:11 +0700

    routing uploadcontroller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminLoginController;
 use App\Http\Middleware\AdminMiddleware;  
+use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
