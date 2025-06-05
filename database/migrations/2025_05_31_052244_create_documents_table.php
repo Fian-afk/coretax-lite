9bc37d4de8ebe5fb59b9bc6c38226e77336ca1f4
Yohanes699 <yohanes0681ft.2024@student.uny.ac.id>
2025-06-06 00:25:18 +0700

    Upload tersimpan di Database, Yohanes

--- database/migrations/2025_05_31_052244_create_documents_table.php
+++ database/migrations/2025_05_31_052244_create_documents_table.php
@@ -20,6 +20,9 @@ return new class extends Migration
         $table->string('instansi')->nullable();
         $table->string('file_path');
         $table->timestamps();
+
+        // Tambahkan foreign key constraint
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
     });
     }
 
